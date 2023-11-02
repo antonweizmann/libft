434c3aa12a6b67060309c57bd172e8987f9869db
antonweizmann <149684961+antonweizmann@users.noreply.github.com>
2023-11-02 13:43:33 +0100

    Add files via upload

---
+++ ft_lstadd_back.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aweizman <aweizman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 19:26:59 by aweizman          #+#    #+#             */
+/*   Updated: 2023/10/11 14:07:26 by aweizman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*tmp;
+
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	tmp = ft_lstlast(*lst);
+	tmp -> next = new;
+}
